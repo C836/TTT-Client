ed19cfa7a605761a5fa4f385fac1d7dce34b0476
C836 <gl.sepiol@gmail.com>
2022-06-12 20:10:21 -0300

    STYLE: Criando container Menu

---
+++ src/components/Join_Menu/Menu.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Menu = styled.section`
+    width: 300px;
+    
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+`
