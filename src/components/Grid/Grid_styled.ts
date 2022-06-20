404ba589cf5d13cc7361d288d0a735e2ba70a81c
C836 <gl.sepiol@gmail.com>
2022-06-20 16:44:17 -0300

    Estilizando Grid do jogo

--- src/components/Grid/Grid_styled.ts
+++ src/components/Grid/Grid_styled.ts
@@ -5,13 +5,18 @@ interface Props {
 }
 
 export const Grid_Styled = styled.section<Props>`
-width: 100%;
-max-width: 300px;
-background-color: white;
+width: 400px;
+max-width: 95%;
+
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
 
 display: grid;
 grid-template-columns: repeat(3, 1fr);
 grid-template-rows: repeat(3, 1fr);
+gap: 10px;
 
 pointer-events: ${({disabled}) => disabled ? "none" : "all"};
 opacity: ${({disabled}) => disabled ? "0.6" : "1"};
